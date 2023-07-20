763ed42f1a36de9ea3a0e46ccb4c88bee433d5b9
p2wna22002 <126845591+p2wna22002@users.noreply.github.com>
2023-07-20 10:28:47 +0530

    Add files via upload

---
+++ LAB1/oddnum.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.8.2 <0.9.0;
+
+contract oddnumcounter {
+    function countoddnum(uint n) public pure returns (uint) {
+        uint count = 0;
+        for (uint i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
